7237e76537d2db21da3bac94d95bfe74da07552c
ROHAN4535 <grohan468@gmail.com>
2023-09-25 23:54:54 +0530

    JSX limitation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
+import { Fragment } from "react";
 
 function App() {
   const [usersList, setUsersList] = useState([]);
@@ -16,10 +17,10 @@ function App() {
   };
 
   return (
-    <div>
+    <Fragment className="App">
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
