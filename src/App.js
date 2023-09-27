2953af08747da0586e5d3b2e6d06922abdba43f1
ROHAN4535 <grohan468@gmail.com>
2023-09-27 15:26:21 +0530

    Refs and Controlled Components

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import React, { useState, Fragment } from 'react';
 
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
-import { Fragment } from "react";
 
 function App() {
   const [usersList, setUsersList] = useState([]);
 
-  const addUserHandler = (uName, uAge) => {
+  const addUserHandler = (uName, uAge, cName ) => {
     setUsersList((prevUsersList) => {
       return [
         ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
+        { name: uName, age: uAge, clgName: cName, id: Math.random().toString() },
       ];
     });
   };
 
   return (
-    <Fragment className="App">
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
     </Fragment>
